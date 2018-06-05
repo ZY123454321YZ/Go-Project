8127257485af6ef28736f9ad1c363db1dba6bf1c
zhaoyong <834162364@qq.com>
2018-06-05 15:45:42 +0800

    Author ZhaoYong

---
+++ Go/src/main/demo.go
@@ -0,0 +1,18 @@
+package main
+
+import (  
+    "fmt"  
+    "os"  
+)  
+  
+func main() {  
+    who := "world!"  
+    if len(os.Args[1:]) > 0 {  
+        who = ""  
+        for _, arg := range os.Args[1:] {  
+            who += " " + arg  
+        }  
+    }  
+    fmt.Println("hello", who)  
+}  
+
